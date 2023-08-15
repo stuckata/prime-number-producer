94849832f11123a2bf3a7f9703f994327749d835
stoyan kirkov <stu.kirkov@gmail.com>
2023-08-15 08:58:06 +0300

    initial commit

---
+++ src/main/java/com/example/primenumberproducer/PrimeNumberProducerApplication.java
@@ -0,0 +1,13 @@
+package com.example.primenumberproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeNumberProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeNumberProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/primenumberproducer/PrimeNumberProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.primenumberproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeNumberProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
